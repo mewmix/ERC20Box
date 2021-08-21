f8844d9c4761e49d9a2014325182bf102655bff7
chubbykat <42463809+mewmix@users.noreply.github.com>
2021-08-21 03:16:21 -0700

    Update Storage.sol

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // changed import
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./TradeableERC721Token.sol"
-contract TradeableERC721 is ERC721URIStorage
+contract TradeableERC721Token is ERC721URIStorage
