d1f267522b908b2f6b5b7a88071b490c8e85a514
chubbykat <42463809+mewmix@users.noreply.github.com>
2021-08-21 03:19:00 -0700

    Update TradeableERC721Token.sol

--- contracts/TradeableERC721Token.sol
+++ contracts/TradeableERC721Token.sol
@@ -10,7 +10,8 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // changed import
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 
