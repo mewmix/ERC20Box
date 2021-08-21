b0739a1eb53a92c844cafc117b14eba4ad8ace6b
chubbykat <42463809+mewmix@users.noreply.github.com>
2021-08-21 03:21:52 -0700

    Update Storage.sol

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,6 +1,32 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // changed import
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "./TradeableERC721Token.sol"
-contract TradeableERC721Token is ERC721URIStorage
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+
+contract TradeableERC721Token is ERC721URIStorage {
+    string baseURI;
+    
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    
+    function mint(
+        address account,
+        uint256 tokenId
+    ) external {
+        _mint(account, tokenId);
+    }
+    
+    function setTokenURI(
+        uint256 tokenId, 
+        string memory tokenURI
+    ) external {
+        _setTokenURI(tokenId, tokenURI);
+    }
+    
+    function setBaseURI(string memory baseURI_) external {
+        baseURI = baseURI_;
+    }
+    
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+}
