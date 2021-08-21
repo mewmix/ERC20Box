9a621e113d3eec2152e75d3944b83ae8ce969697
chubbykat <42463809+mewmix@users.noreply.github.com>
2021-08-21 03:22:13 -0700

    Update Storage.sol

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.1;
+pragma solidity ^0.8.0;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
